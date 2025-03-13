0367c563305e50b11b73aa55ff20a0efd17df67e
nitish <nitishkhagaria7@gmail.com>
2025-03-13 14:04:27 +0530

    first commit

---
+++ src/main/java/com/testConflict/TestConflictApplication.java
@@ -0,0 +1,17 @@
+package com.testConflict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestConflictApplication {
+
+	public static void main(String[] args) {
+		int x=98;
+		char ch='x';
+		String s= "nitish";
+		boolean b=true;
+		SpringApplication.run(TestConflictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testConflict/TestConflictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testConflict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestConflictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
